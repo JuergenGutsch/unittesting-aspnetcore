954ae3c90983678e49981a22c6dc05a356a5b29a
JuergenGutsch <juergen@gutsch-online.de>
2017-12-14 21:25:11 +0100

    fix deployment script

--- deploy-to-azure.ps1
+++ deploy-to-azure.ps1
@@ -1,12 +1,12 @@
 
-$uri = "https://$$env:SITE_NAME:$env:FTP_PASSWORD@$env.SITE_NAME.scm.azurewebsites.net/deploy?scmType=GitHub"
+$uri = "https://`$$Env:SITE_NAME:$Env:FTP_PASSWORD@$Env:SITE_NAME.scm.azurewebsites.net/deploy?scmType=GitHub"
 $headers = @{ }
 $headers.Add("Content-Type", "application/json")
 $headers.Add("Accept", "application/json")
-$headers.Add("X-SITE-DEPLOYMENT-ID", $env:SITE_NAME)
+$headers.Add("X-SITE-DEPLOYMENT-ID", $Env:SITE_NAME)
 $headers.Add("Transfer-encoding", "chunked")
 $body = @{ }
 $body.Add("format", "basic")
-$body.Add("url", "https://$env:GITHUB_USERNAME:$env:GITHUB_PASSWORD@GITHUB.org/$env:GITHUB_USERNAME/$env:REPOSITORY_NAME.git")
+$body.Add("url", "https://$Env:GITHUB_USERNAME:$Env:GITHUB_PASSWORD@GITHUB.org/$Env:GITHUB_USERNAME/$Env:REPOSITORY_NAME.git")
 
 Invoke-WebRequest -Uri $uri -Method "POST" -Headers $headers -Body $body
